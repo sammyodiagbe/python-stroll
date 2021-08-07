5c16648d4757e7c822f526fe795bc080b4ee4b24
Odiagbe Samson Osaro <odiagbesamsonosaro@gmail.com>
2021-08-07 21:26:58 +0100

    python stroll day 3

---
+++ 03/01 modules.py
@@ -0,0 +1,4 @@
+import random as rand
+
+for i in range(5):
+    print(rand.randint(1, 15))
---
+++ 03/02 exiting_progam.py
@@ -0,0 +1,8 @@
+import sys
+
+while True:
+    print("Type exit to quit.")
+    response = input()
+    if response == 'exit':
+        sys.exit()
+    print("You have quit the progam")
