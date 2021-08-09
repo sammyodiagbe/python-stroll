652355e0482f9f5745240b84cd36ff0c448b8345
Odiagbe Samson Osaro <odiagbesamsonosaro@gmail.com>
2021-08-09 20:35:01 +0100

    learning to use python

---
+++ 05/02 Tuple.py
@@ -0,0 +1,5 @@
+# a tuple is almost the same as a list but we create tuples with () or the tuple class
+# tuples are immutable, meaning there values cannot be changed
+
+my_tuple = tuple([1,4,5,6,78,3])
+print(my_tuple)
