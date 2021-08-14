e40a7f7a92c56c7388593cee03f5be85a1cd9287
Odiagbe Samson Osaro <odiagbesamsonosaro@gmail.com>
2021-08-14 21:34:14 +0100

    learning about optional matches

--- 09/01 regular_expression.py
+++ 09/01 regular_expression.py
@@ -1,5 +1,6 @@
 import re
 
+general_text = 'Hey, give me a call at 8;46 on my cell no +1-555-222-2323 1-555-222-2323'
 phone_regex = r'\d\d\d-\d\d\d-\d\d\d\d'
 print(phone_regex)
 mo = re.findall(phone_regex, 'This is my number 123-123-1234')
@@ -7,5 +8,11 @@ print('Phone number found: ', mo)
 
 advanced_regex_pattern = r'\+\d{1}-\d{3}-\d{3}-\d{4}'
 result = re.findall(advanced_regex_pattern,
-                    'Hey, give me a call at 8;46 on my cell no +1-555-222-2323')
+                    'Hey, give me a call at 8;46 on my cell no +1-555-222-2323 1-555-222-2323')
 print(result)
+
+
+#  optional matches
+optional_regex = '\+?\d{1}-\d{3}-\d{3}-\d{4}'
+optional_result = re.findall(optional_regex, general_text)
+print(optional_result)
