e6ad91b406cdab6d4e420011a87c905bc1e2aca6
Odiagbe Samson Osaro <odiagbesamsonosaro@gmail.com>
2021-08-14 21:24:06 +0100

    understanding regular expression

--- 09/01 regular_expression.py
+++ 09/01 regular_expression.py
@@ -1,6 +1,11 @@
 import re
 
-phone_regex = re.compile('\d\d\d-\d\d\d-\d\d\d\d')
+phone_regex = r'\d\d\d-\d\d\d-\d\d\d\d'
 print(phone_regex)
-mo = phone_regex.search('My number is 123-444-555-666')
+mo = re.findall(phone_regex, 'This is my number 123-123-1234')
 print('Phone number found: ', mo)
+
+advanced_regex_pattern = r'\+\d{1}-\d{3}-\d{3}-\d{4}'
+result = re.findall(advanced_regex_pattern,
+                    'Hey, give me a call at 8;46 on my cell no +1-555-222-2323')
+print(result)
