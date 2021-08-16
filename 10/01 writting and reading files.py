e85522bec1444d0940a21f4923e2c15ccf67c067
Odiagbe Samson Osaro <odiagbesamsonosaro@gmail.com>
2021-08-16 16:46:14 +0100

    getting total size of docs in a directory

--- 10/01 writting and reading files.py
+++ 10/01 writting and reading files.py
@@ -4,3 +4,12 @@ print(os.path.join('usr', 'bin', 'spam'))
 print(os.getcwd())
 # os.makedirs('./hello.txt')
 print(os.path.abspath('.'))
+print(os.path.getsize("./hello.txt"))
+print(os.listdir("."))
+
+totalsize = 0
+# getting the combined size of all dirs and files in this dir
+for filename in os.listdir("."):
+    totalsize += os.path.getsize(os.path.join('.', filename))
+
+print(totalsize)
